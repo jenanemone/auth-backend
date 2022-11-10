d89268ed74451f92667b68718c8a03343ec26810
Jennie Allen <jenanemone@gmail.com>
2022-11-10 17:01:28 -0600

    working for local with Mongo

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -11,7 +11,6 @@ async function dbConnect() {
         //   these are options to ensure that the connection is done properly
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
       }
     )
     .then(() => {
